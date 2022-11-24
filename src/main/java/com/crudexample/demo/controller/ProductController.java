8d8ccd590f51a67a9ebb6969f436a93fb9c27878
VINOTH <vinothcand2000@gmail.com>
2022-11-24 12:09:50 +0000

    Exception Handler

--- src/main/java/com/crudexample/demo/controller/ProductController.java
+++ src/main/java/com/crudexample/demo/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.crudexample.demo.controller;
 
+import com.crudexample.demo.customException.BusinessException;
+import com.crudexample.demo.customException.ControllerException;
 import com.crudexample.demo.entity.Product;
 import com.crudexample.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,18 +16,41 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+//    @PostMapping("/addProduct")
+//    public Product addProduct(@RequestBody Product product) {
+//        try {
+//            return productService.saveProduct(product);
+//        } catch (BusinessException e) {
+//            ControllerException c = new ControllerException(e.getErrorCode(), e.getErrorMessage());
+//
+//        } catch (Exception e) {
+//
+//        }
+//    }
+
     @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product){
-        return productService.saveProduct(product);
+    public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        try {
+            Product p = productService.saveProduct(product);
+            return new ResponseEntity<Product>(p, HttpStatus.CREATED);
+            // return productService.saveProduct(product);
+        } catch (BusinessException e) {
+            ControllerException c = new ControllerException(e.getErrorCode(), e.getErrorMessage());
+            return new ResponseEntity<ControllerException>(c, HttpStatus.BAD_REQUEST);
+
+        } catch (Exception e) {
+            ControllerException c = new ControllerException("611", "Something went wrong in Controller");
+            return new ResponseEntity<ControllerException>(c, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products){
+    public List<Product> addProducts(@RequestBody List<Product> products) {
         return productService.saveProducts(products);
     }
 
     @GetMapping("/products")
-    public List<Product> findAllProducts(){
+    public List<Product> findAllProducts() {
         return productService.getProducts();
     }
 
@@ -33,17 +60,17 @@ public class ProductController {
     }
 
     @GetMapping("/productByName/{name}")
-    public Product findProductByName(@PathVariable String name){
+    public Product findProductByName(@PathVariable String name) {
         return productService.getProductByName(name);
     }
 
     @PutMapping("/update")
-    public Product updateProduct(@RequestBody Product product){
+    public Product updateProduct(@RequestBody Product product) {
         return productService.updateProduct(product);
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable int id){
+    public String deleteProduct(@PathVariable int id) {
         return productService.deleteProduct(id);
     }
 
--- src/main/java/com/crudexample/demo/service/ProductService.java
+++ src/main/java/com/crudexample/demo/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.crudexample.demo.service;
 
+import com.crudexample.demo.customException.BusinessException;
 import com.crudexample.demo.entity.Product;
 import com.crudexample.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,32 +13,42 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
-    public Product saveProduct(Product product){
-        return repository.save(product);
+    public Product saveProduct(Product product) {
+        if (product.getName().isEmpty() || product.getName().length() == 0) {
+            throw new BusinessException("601", "Product Name is Empty");
+        }
+        try {
+            return repository.save(product);
+        } catch (IllegalArgumentException e) {
+            throw new BusinessException("602", "Given Product is null" + e.getMessage());
+
+        } catch (Exception e) {
+            throw new BusinessException("603", "Some Other Exception in Product Save Service Layer" + e.getMessage());
+        }
     }
 
-    public List<Product> saveProducts(List<Product> products){
+    public List<Product> saveProducts(List<Product> products) {
         return repository.saveAll(products);
     }
 
-    public List<Product> getProducts(){
+    public List<Product> getProducts() {
         return repository.findAll();
     }
 
-    public Product getProductById(int id){
+    public Product getProductById(int id) {
         return repository.findById(id).orElse(null);
     }
 
-    public Product getProductByName(String name){
+    public Product getProductByName(String name) {
         return repository.findByName(name);
     }
 
-    public String deleteProduct(int id){
+    public String deleteProduct(int id) {
         repository.deleteById(id);
-        return "Product"+" "+ id + " " + "Deleted Successfully";
+        return "Product" + " " + id + " " + "Deleted Successfully";
     }
 
-    public Product updateProduct(Product product){
+    public Product updateProduct(Product product) {
         Product exisitingProduct = repository.findById(product.getId()).orElse(null);
         exisitingProduct.setName(product.getName());
         exisitingProduct.setQuantity(product.getQuantity());
